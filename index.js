d3d0cda123c47cb423fff592187072e56f39d68e
oharato <oharato@live.jp>
2022-05-04 00:34:50 +0900

    update path

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const app = new Vue({
     }
   },
   async created() {
-    const url = '/store_list.csv'
+    const url = './store_list.csv'
     const res = await fetch(url);
     const text = await res.text();
     this.stores = text.split('\n').slice(1).map(l => new Store(...l.split(',')));
